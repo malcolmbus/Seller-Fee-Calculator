aaffedb1883b1012ac09b050fe209c004e8f8f3f
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-06-15 15:09:04 -0400

    Updated HTML and implemented calculateFees method to calculate the fees and profit expected when the Calculate button is clicked.

--- script.js
+++ script.js
@@ -22,76 +22,58 @@ function openTab(storeName, tab){
 
   if(storeName == "grailed"){
     document.getElementById("radio-container").style.display = "block";
+    document.getElementById("calcbutton").style.display = "block";
+    document.getElementById("calcbutton2").style.display = "none";
   }
   else{
     document.getElementById("radio-container").style.display = "none";
+    document.getElementById("calcbutton").style.display = "none";
+    document.getElementById("calcbutton2").style.display = "block";
   }
+
   clearForm();
   }
 
   function clearForm(){
     document.getElementById("listprice").value = "";
     document.getElementById("shipcost").value = "";
+    document.getElementById("sfeesresult").textContent ="--";
+    document.getElementById("pfeesresult").textContent = "--";
+    document.getElementById("profitresult").textContent = "--";
+    var selectedradio = document.getElementsByName("location");
+    for(var i = 0; i < selectedradio.length; i++){
+      selectedradio[i].checked = false;
+    }
   }
 
-  // function calculateFees(listPrice, shipCost, currency, translocation){
-  //   var sellerFee;
-  //   var paypalFee;
-  //   var currencySymbol, sellerFeeResult, paypalFeeResult, totalProfit;
-  //   var grailed = document.getElementById("grailed");
-  //   var depop = document.getElementById("depop");
-  //
-  //   if(grailed.style.display = "block"){
-  //     sellerFee = 0.09;
-  //     if(document.getElementById("domestic").checked){
-  //       paypalFee = 0.029 + 0.30;
-  //     }
-  //     else if(document.getElementById("international").checked){
-  //       paypalFee = 0.44 + 0.30;
-  //     }
-  //   }
-  //   else if(depop.style.display = "block"){
-  //     sellerFee = 0.1;
-  //     payPalFee = 0.029 + 0.30;
-  //   }
-  //
-  //   if(currency == "USD"){
-  //     currencySymbol = "$";
-  //   }
-  //   else if(currency == "GBP"){
-  //     currencySymbol = "£";
-  //   }
-  //   else if(currency == "CAD"){
-  //     currencySymbol = "CAD";
-  //   }
-  //
-  //   sellerFeeResult = listPrice - shipCost * sellerFee;
-  //   paypalFeeResult = listPrice - shipCost * payPalFee;
-  //   totalProfit = listPrice - sellerFeeResult - paypalFeeResult;
-  //
-  //   document.getElementById("sfeesresult").innerHTML = sellerFeeResult;
-  //   document.getElementById("pfeesresult").innerHTML = payPalFeeResult;
-  //   document.getElementById("profitresult").innerHTML = currencySymbol + " " + totalProfit;
-  // }
+  function calculateFees(storeName){
+    var listPrice = document.getElementById("listprice").value;
+    var shipCost = document.getElementById("shipcost").value;
+    var sellerFee;
+    var paypalFee;
+    var currencySymbol, sellerFeeResult, paypalFeeResult, totalProfit;
+    var grailed = document.getElementById("grailed");
+    var depop = document.getElementById("depop");
 
-//   var listPrice = document.getElementById("listprice").value;
-//   var shipCost = document.getElementById("shipcost").value;
-//   var currency = document.getElementById("currency").selectedIndex.value;
-//   var transLocation;
-//   if(document.getElementById("domestic").checked){
-//     transLocation = document.getElementById("domestic").value;
-//   }
-//   else if(document.getElementById("international").checked){
-//     transLocation = document.getElementById("international").value;
-//   }
-//
-//   document.getElementById("calcbutton")
-//   .addEventListener("click", calculateFees(listPrice, shipCost, currency, transLocation));
-// }
+    if(storeName == "grailed"){
+      sellerFee = 0.09
+      if(document.getElementById("domestic").checked){
+        paypalFee = 0.029;
+      }
+      else if(document.getElementById("international").checked){
+        paypalFee = 0.044;
+      }
+    }
+    else if(storeName == "depop"){
+      sellerFee = 0.1;
+      paypalFee = 0.029;
+    }
 
+    sellerFeeResult = parseFloat((listPrice - shipCost) * sellerFee);
+    paypalFeeResult = parseFloat((listPrice - shipCost) * paypalFee + 0.30);
+    totalProfit = parseFloat(listPrice - sellerFeeResult - paypalFeeResult);
 
-
-
-                          /*TO DO*/
-  // add a description of the fees below the form to be changed on each page
-  // add a clear button ?
+    document.getElementById("sfeesresult").textContent = sellerFeeResult.toFixed(2);
+    document.getElementById("pfeesresult").textContent = paypalFeeResult.toFixed(2);
+    document.getElementById("profitresult").textContent = totalProfit.toFixed(2);
+  }
